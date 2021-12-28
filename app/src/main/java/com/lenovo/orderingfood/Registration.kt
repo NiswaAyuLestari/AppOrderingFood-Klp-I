77512218e5e730a56fc3f4decb9abb9d4e1f4d91
DESKTOP-EO39B50\LENOVO PC <niswaayu99@gmail.com>
2021-12-28 21:25:10 +0700

    pembuatan login, registrasi, home awal 1

--- app/src/main/java/com/lenovo/orderingfood/Registration.kt
+++ app/src/main/java/com/lenovo/orderingfood/Registration.kt
@@ -1,11 +1,20 @@
 package com.lenovo.orderingfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val textLogin: TextView = findViewById(R.id.tv_sign_in)
+        textLogin.setOnClickListener {
+            val intentLogin = Intent(this, Login::class.java)
+            startActivity(intentLogin)
+        }
     }
 }
--- app/src/main/java/com/lenovo/orderingfood/SlashScreen.kt
+++ app/src/main/java/com/lenovo/orderingfood/SlashScreen.kt
@@ -1,11 +1,26 @@
 package com.lenovo.orderingfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class SlashScreen : AppCompatActivity() {
+
+class SlashScreen : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slash_screen)
-    }
+
+        val buttonLogin: Button = findViewById(R.id.bt_login)
+        buttonLogin.setOnClickListener {
+            val intentLogin = Intent(this, Login::class.java)
+            startActivity(intentLogin)
+        }
+
+        val buttonRegis: Button = findViewById(R.id.bt_regis)
+            buttonRegis.setOnClickListener{
+                val intenRegis = Intent(this, Registration::class.java)
+                startActivity(intenRegis)
+            }
+        }
 }
